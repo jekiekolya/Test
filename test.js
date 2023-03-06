2f54f25f8dbf27f3fc345f9221b014adfae71335
Mykola Berezhniuk <108493349+jekiekolya@users.noreply.github.com>
2023-03-07 00:00:22 +0200

    Update test.js

--- test.js
+++ test.js
@@ -121,28 +121,116 @@
 
 // let shogun = new Samurai('Dimych');
 // console.log(shogun.__proto__.__proto__ === Object.prototype);
-// console.log(shogun);
+// console.log(shogun.__proto__ === Samurai.prototype);
 // console.log(shogun.__proto__.constructor.__proto__ === Function.prototype);
 // console.log(shogun.__proto__.__proto__.__proto__ === null);
 
-var isAnagram = function (test, original) {
-  if (test.length !== original.length) {
-    return false;
-  }
+//  -----------------------------------------------------------------------------
+// Ex 1:
+// > abc
+// [ ‘abc’, ‘a.bc’, ‘ab.c’, ‘a.b.c’]
 
-  const arrTest = test.toLowerCase().split('');
-  const arrOriginal = original.toLowerCase().split('');
+// > abcd
+// [ ‘abcd’, ‘a.bcd’, ‘ab.cd’, ‘a.b.cd’ … ]
 
-  for (const item of arrTest) {
-    const index = arrOriginal.indexOf(item);
-    if (index !== -1) {
-      arrOriginal.splice(index, 1);
-    }
-  }
+// function createArray(str) {
+//   const resultArr = [];
+//   resultArr.push(str);
 
-  return arrOriginal.length === 0;
+//   for (let i = 1; i <= str.length - 1; i++) {
+//     if (i >= 3) {
+//       let newStr = str.split('').slice(0, i);
+//       let newStr2 = str.slice(i, str.length);
+//       newStr = newStr.join('.');
+//       resultArr.push(newStr + newStr2);
+//     }
+
+//     let newStr = str.split('');
+
+//     newStr.splice(i, 0, '.');
+//     newStr = newStr.join('');
+//     resultArr.push(newStr);
+//   }
+
+//   resultArr.push(str.split('').join('.'));
+//   return resultArr;
+// }
+
+// console.log('createArray(adc)', createArray('abc'));
+// console.log('createArray(abcde)', createArray('abcde'));
+
+//  -----------------------------------------------------------------------------
+// Ex 2:
+// 1. Given unit test. Create chunk method that suits given unit test
+
+// describe("Array Chunking", () => {
+
+//   it("Should implement array chunking", () => {
+
+//    assert.deepEqual(chunk([1, 2, 3, 4], 2), [[1, 2], [3, 4]]);
+//    assert.deepEqual(chunk([1, 2, 3, 4], 3), [[1, 2, 3], [4]]);
+//    assert.deepEqual(chunk([1, 2, 3, 4], 5), [[1, 2, 3, 4]]);
+//    assert.deepEqual(chunk([1, 2, 3, 4, 5, 6], 2), [[1, 2], [3, 4], [5, 6]]);
+//   });
+// });
+
+// function chunk(arr, size) {
+//   const newArr = [];
+//   for (let i = 0; i < arr.length; ) {
+//     newArr.push(arr.splice(i, size));
+//     i = 0;
+//   }
+
+//   return newArr;
+// }
+
+// console.log('chunk([1, 2, 3, 4], 2', chunk([1, 2, 3, 4], 2));
+// console.log('chunk([1, 2, 3, 4], 3', chunk([1, 2, 3, 4], 3));
+// console.log('chunk([1, 2, 3, 4], 5', chunk([1, 2, 3, 4], 5));
+// console.log('chunk([1, 2, 3, 4, 5, 6], 2', chunk([1, 2, 3, 4, 5, 6], 2));
+
+//  -----------------------------------------------------------------------------
+// Ex 3: Palindrome
+
+// function isPalindrome(str) {
+//   const newStr = str.split('').reverse().join('');
+
+//   return newStr === str;
+// }
+
+// console.log('isPalindrome(add)', isPalindrome('add'));
+// console.log('isPalindrome(ada)', isPalindrome('ada'));
+
+//  -----------------------------------------------------------------------------
+// Ex 3: FindShortestWord
+
+// const findShort = str => {
+//   const arr = str.split(' ');
+//   const shortestWord = arr.reduce((acc, item) => {
+//     if (acc.length > item.length) {
+//       return item;
+//     } else {
+//       return acc;
+//     }
+//   }, arr[0]);
+
+//   return shortestWord;
+// };
+
+// console.log('findShort(The Smallest word in sentence)', findShort('The Smallest word in sentence'));
+// console.log('findShort(Just test string)', findShort('Just test string'));
+
+//  -----------------------------------------------------------------------------
+// Ex 4: toInitials
+
+const toInitials = str => {
+  const arr = str.toUpperCase().split(' ');
+  const initials = arr.reduce((acc, item) => {
+    return acc + `${item[0]}.`;
+  }, '');
+
+  return initials;
 };
-console.log(isAnagram('dumble', 'bumble'));
 
-console.log(asd);
-let asd = 12;
+console.log('toInitials(Bill Gates)', toInitials('Bill Gates'));
+console.log('toInitials(elo mask)', toInitials('elo mask'));
