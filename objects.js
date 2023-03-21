7398e8becf394c580a49b2c4d9dbf29b8cc40413
Mykola Berezhniuk <108493349+jekiekolya@users.noreply.github.com>
2023-03-21 15:05:19 +0200

    Update objects.js

--- objects.js
+++ objects.js
@@ -1,10 +1,44 @@
+// const person = {
+//   greet() {
+//     console.log(`Hello, my name is ${this.name}`);
+//   },
+// };
+
+// const john = Object.create(new Number(1));
+// const john2 = Object.create({});
+// const john3 = Object.create(1);
+// const john4 = Object.create([1]);
+// john.age = 1;
+// john.name = 'John';
+// console.log('john', john);
+// console.dir(john);
+
+// const john = Object.create(person, {
+//   name: {
+//     value: 'John',
+//     writable: true,
+//     enumerable: true,
+//     configurable: true,
+//   },
+//   age: {
+//     value: 30,
+//     writable: false,
+//     enumerable: true,
+//     configurable: false,
+//   },
+// });
+
 const person = {
-  greet() {
-    console.log(`Hello, my name is ${this.name}`);
-  },
+  name: 'John',
+  age: 30,
+  gender: 'male',
 };
 
-const john = Object.create(person);
-john.name = 'John';
+const details = {
+  occupation: 'engineer',
+  location: 'New York',
+};
 
-john.greet(); // Output: Hello, my name is John
+const personDetails = Object.assign({}, person, details);
+
+console.log(personDetails);
